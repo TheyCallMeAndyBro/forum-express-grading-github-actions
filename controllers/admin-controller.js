b449e5403242395e546400ce0ab9d622f418b488
TheyCallMeAndyBro <andy060104@gmail.com>
2023-11-06 16:46:01 +0800

    add categories selector on admin create and edit page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -11,18 +11,22 @@ const adminController = {
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
   },
-  createRestaurant: (req, res) => {
-    res.render('admin/create-restaurant')
+  createRestaurant: (req, res, next) => {
+    return Category.findAll({
+      raw: true
+    })
+      .then(categories => res.render('admin/create-restaurant', { categories }))
+      .catch(err => next(err))
   },
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
     if (!name) throw new Error('Restaurant name is required !')
 
     const { file } = req // 這邊要獲取的是req中file這個檔案 而req.body.file 獲取的是 name=file 輸入的字串
 
     localFileHandler(file) // 傳入req獲取的file 有無檔案邏輯已在helpers內做完
       .then(filePath => { // filePath為最後resolve的`/${fileName}`路徑字符串
-        return Restaurant.create({ name, tel, address, openingHours, description, image: filePath || null })
+        return Restaurant.create({ name, tel, address, openingHours, description, image: filePath || null, categoryId })
       })
 
       .then(() => {
@@ -45,18 +49,19 @@ const adminController = {
       .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, {
-      raw: true
-    })
-      .then(restaurant => {
+    return Promise.all([
+      Restaurant.findByPk(req.params.id, { raw: true }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurant, categories]) => {
         if (!restaurant) throw new Error('Restaurant did not exist!')
 
-        res.render('admin/edit-restaurant', { restaurant })
+        res.render('admin/edit-restaurant', { restaurant, categories })
       })
       .catch(err => next(err))
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
     if (!name) throw new Error('Restaurant name is required !')
 
     const { file } = req // 這邊要獲取的是req中file這個檔案 而req.body.file 獲取的是 name=file 輸入的字串
@@ -71,7 +76,7 @@ const adminController = {
         if (!restaurant) throw new Error('Restaurant did not exist!')
         // 也可用Restaurant.update寫法 裡面多添加where 就好
         // image: filePath || restaurant.image 如果有編輯照片 就用新的 沒有用舊的
-        return restaurant.update({ name, tel, address, openingHours, description, image: filePath || restaurant.image })
+        return restaurant.update({ name, tel, address, openingHours, description, image: filePath || restaurant.image, categoryId })
       })
 
       .then(() => {
